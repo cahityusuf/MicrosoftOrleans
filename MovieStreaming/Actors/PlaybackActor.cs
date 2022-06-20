edc9933a219597ecd3ed1be32486e63e0ead9477
Cahit Yusuf KAFADAR <cahityusuf@gmail.com>
2022-06-20 03:04:58 +0300

    Actor Lifecycle Messages

--- MovieStreaming/Actors/PlaybackActor.cs
+++ MovieStreaming/Actors/PlaybackActor.cs
@@ -1,18 +1,56 @@
+using MovieStreaming;
 using MovieStreaming.Messages;
 using Proto;
 
-public class PlaybackActor : IActor
+namespace MovieStreaming.Actors
 {
-    public PlaybackActor() => Console.WriteLine("Creating a PlaybackActor");
-    public Task ReceiveAsync(IContext context)
+    public class PlaybackActor : IActor
     {
-        switch (context.Message)
+        public PlaybackActor() => Console.WriteLine("Creating a PlaybackActor");
+        public Task ReceiveAsync(IContext context)
         {
-            case PlayMovieMessage msg:
-                Console.WriteLine($"Received movie title {msg.MovieTitle}");
-                Console.WriteLine($"Received user ID {msg.UserId}");
-                break;
+            switch (context.Message)
+            {
+                case Started msg:
+                    ProcessStartedMessage(msg);
+                    break;
+
+                case PlayMovieMessage msg:
+                    ProcessPlayMovieMessage(msg);
+                    break;
+
+                case Recoverable msg:
+                    ProcessRecoverableMessage(context, msg);
+                    break;
+            }
+            return Task.CompletedTask;
         }
-        return Task.CompletedTask;
+
+        private void ProcessStartedMessage(Started msg)
+        {
+            ColorConsole.WriteLineGreen("PlaybackActor Started");
+        }
+
+        private void ProcessPlayMovieMessage(PlayMovieMessage msg)
+        {
+            ColorConsole.WriteLineYellow($"PlayMovieMessage {msg.MovieTitle} for user {msg.UserId}");
+        }
+
+        private void ProcessRecoverableMessage(IContext context, Recoverable msg)
+        {
+            PID child;
+
+            if (context.Children == null || context.Children.Count == 0)
+            {
+                var props = Props.FromProducer(() => new ChildActor());
+                child = context.Spawn(props);
+            }
+            else
+            {
+                child = context.Children.First();
+            }
+
+            context.Forward(child);
+        }
     }
 }
